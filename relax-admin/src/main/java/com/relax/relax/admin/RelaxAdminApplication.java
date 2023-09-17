0c351574a80d82c6e3ff9c2bbd98ce65b9358b79
828470 <812159920@qq.com>
2023-09-17 18:27:40 +0800

    添加前端vue2框架

---
+++ relax-admin/src/main/java/com/relax/relax/admin/RelaxAdminApplication.java
@@ -0,0 +1,13 @@
+package com.relax.relax.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelaxAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RelaxAdminApplication.class, args);
+    }
+
+}
